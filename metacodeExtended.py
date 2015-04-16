f1786b5744fc491d70a8a9eb21f1f46271eff4d3
pierrestefani <poubelledepierre@gmail.com>
2015-04-16 22:00:41 +0200

    Implémentation d'un parcours pour la diffusion et l'absorption

--- metacodeExtended.py
+++ metacodeExtended.py
@@ -1,41 +1,51 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sun Mar 29 15:47:35 2015
-
 @author: ubuntu
 """
-
+import pyAgrum as gum
+import gumLib.notebook as gnb
 from collections import Counter
 from itertools import chain, islice
 
 
-#Génère des noms de potentiel
 def nomPotentiel(jt,c):
+    '''Génère des noms de potentiel pour la clique c d'un arbre de jonction'''
     res="Phi"
     for n in jt.clique(c):
         res += str(n)+"_"
     return res
     
-#Retourne le nom des potentiels associés aux évidences
-def nomPotentielEvs(bn, evs):    
+def nomPotentielEvs(bn, evs):  
+    '''Retourne le nom des potentiels associés aux évidences'''
     res=list()
     for i in bn.ids():
         if(evs.has_key(bn.variable(i).name())):
             res.append(i)
     return res
     
-#Retourne la clique "racine" vers laquelle on fait converger l'information
-def cliqueRacine(jt):
-    c = Counter(list(chain(*jt.edges())))
-    return c.most_common(1)[0][0]
+    
+def cliqueRacine(bn,jt,target):
+    '''Retourne la clique "racine" vers laquelle on fait converger l'information'''
+    maxi = -1 #contient le nombre de voisins de la clique contenant le plus de voisins et contenant au moins une target
+    for i in jt.ids():
+        for j in target:
+            if(bn.idFromName(j) in jt.clique(i)):
+                if(nbneighbors(jt,i) > maxi):
+                    maxi = nbneighbors(jt,i)
+                    res = i
+    return res
 
-#Découpe les arcs en liste de couples    
+  
 def couples_cliques(iterable, taille, format=iter):
+    '''Découpe les arcs en liste de couples  '''
     it = iter(iterable)
     while True:
         yield format(chain((it.next(),), islice(it, taille - 1)))
     
+    
 def neighbors(jt,c):
+    '''Renvoie la liste des cliques voisines de la clique c dans l'arbre de jonction jt'''
     ls = jt.edges()
     res = []
     for i in ls:
@@ -45,13 +55,14 @@ def neighbors(jt,c):
             res.append(i[0])
     return res
 
-#Retourne le nombre de voisins de la clique c
+
 def nbneighbors(jt,c):
-        return len(neighbors(jt,c))
+    '''Retourne le nombre de voisins de la clique c'''
+    return len(neighbors(jt,c))
 
 
-#Retourne le nom du séparateur entre deux cliques a et b d'un arbre de jonction jt
 def nom_separateur(jt, ca, cb):    
+    '''Retourne le nom du séparateur entre deux cliques a et b d'un arbre de jonction jt'''
     res="Psi"
     for n in list(jt.clique(ca)):
         res += str(n)+"_"
@@ -61,24 +72,9 @@ def nom_separateur(jt, ca, cb):
     return res
 
 
-def idsOfTargets(bn, target):
-    """Retourne les ids de chaque target donnée pour le réseau bayésien considéré"""
-    res=list()
-    for i in bn.ids():
-        if (bn.variable(i).name() in target):
-            res.append(i)
-    return res
-
-
-def cliquesOfTargets(jt,bn,target):
-    """Retourne le couple [target, clique de la target considéré]"""
-    res = []
-    sel = []
-    for c in jt.ids():
-        for n in jt.clique(c):
-            if (n in idsOfTargets(bn,target) and (n not in sel)):
-                res.append([n,c])
-                sel.append(n)
-    return res
-
-
+def isTarget(bn,jt,target,n):
+    '''Retourne vraie si il existe une target dans la clique n, faux sinon'''
+    for i in target:
+        if(bn.idFromName(i) in jt.clique(n)):
+            return True
+    return False
