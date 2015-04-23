75b0e5ac88402ca6742705f3ea0e6470cee86b7d
pierrestefani <poubelledepierre@gmail.com>
2015-04-23 11:33:13 +0200

    Générateur d'un code descriptif

--- debugGenerator.py
+++ debugGenerator.py
@@ -1,46 +1,50 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Mar 24 14:39:48 2015
-
 @author: Marvin
 """
-import metacodeExtended as mce
-
 # API DE GENERATION DE CODE
-class Generation:
-    def __init__(self):
-        print("Je sais pas quoi mettre là dedans")
+from AbstractGenerator import AbstractGenerator
+
+class debugGenerator(AbstractGenerator):    
+    def creaPot(self,nompot):
+        return ("\tCreation de potentiel :" + str(nompot)+"\n")
+    
+    def addVarPot(self,var,nompot):
+        return("\tAjout de la variable "+str(var)+" au potentiel "+str(nompot)+"\n")
         
-    def Marginalisation(self,cliq,var):
-        raise(NotImplemented)
-
-    def Multiplication(self,sep2,cliq,sep):
-        raise(NotImplemented)
+    def mulPotCpt(self,nompot, var):
+        return("\tMultiplication du potentiel "+str(nompot)+" par la cpt de la variable "+str(var)+"\n")
     
-    def CreationPotentiel(self,jt,c):
-        raise(NotImplemented)
+    def mulPotPot(self,nompot1,nompot2):
+        return("\tMultiplication du potentiel "+str(nompot1)+" par le potentiel "+str(nompot2)+"\n")
         
-    def AjoutVariablePotentiel(self,jt,c):
-        raise(NotImplemented)
-
-
-class CompilGeneration(Generation):
-    def Marginalisation(self,cliq,var):
-        print("Marginalisation de "+cliq+" selon "+var)
-
-    def Multiplication(self,sep2,cliq,sep):
-        print("MULT")
+    def margi(self,nompot1,nompot2):
+        return("\tMarginalisation de "+str(nompot1)+ " par "+str(nompot2)+"\n")
+        
+    def norm(self, nompot):
+        return("\tNormalisation de "+str(nompot)+"\n")
     
-    def CreationPotentiel(self,jt,c):
-        print("Creation du potentiel : "+ mce.nomPotentiel(jt,c))
+    def fill(self, pot, num): #??? Que fait fill dans inférence à la main ???
+        return("\tFill"+str(num)+" de "+str(pot))
     
-    def AjoutVariablePotentiel(self,jt,c,var):
-        print("Ajout de la variable "+str(var)+" au potentiel " +mce.nomPotentiel(jt,c))
-    
-    #def AjoutEvidencePotentiel(self,jt,c,)
-    
-    
-
-    
-
-#hiérarchiser, debug gen et pyAgrum gen
+    def genere(self, comp, nomfichier, nomfonc):
+        flux = open(nomfichier,'w')
+        flux.write("def "+nomfonc+"():\n")
+        for cur in comp:
+            act = cur[0]
+            if act == 'CPO':
+                flux.write(self.creaPot(cur[1]))
+            elif act == 'ADV':
+                flux.write(self.addVarPot(cur[1],cur[2]))
+            elif act == 'MUC':
+                flux.write(self.mulPotCpt(cur[1],cur[2]))
+            elif act == 'MUL':
+                flux.write(self.mulPotPot(cur[1],cur[2]))
+            elif act == 'MAR':
+                flux.write(self.margi(cur[1],cur[2]))
+            elif act == 'NOR':
+                flux.write(self.norm(cur[1]))
+            elif act == 'FIL':
+                flux.write(self.fill(cur[1],cur[2]))
+        flux.close()
