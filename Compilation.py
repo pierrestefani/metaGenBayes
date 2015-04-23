32d20c46867ab53a7ad25ee220356a5fe6b86e8c
pierrestefani <poubelledepierre@gmail.com>
2015-04-23 17:49:41 +0200

    Classe Compilation pour la gestion du tableau d'instructions

---
+++ Compilation.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 23 17:21:42 2015
+
+@author: ubuntu
+"""
+
+class Compilation:
+    
+    def __init__(self):
+        '''La classe Compilation sera un tableau '''        
+        self.tab = []
+        
+    def creerPotentielClique(self,cliq):
+        self.tab.append(["CPO", cliq])
+        
+    def assigneVariablePotentiel(self,var,cliq):
+        self.tab.append(["ADV", var, cliq])      
+    
+    def fillPotentiel(self, cliq, value):
+        self.tab.append(["FIL", cliq, value])
+
+    def multiplicationCPT(self, cliq, cpt):
+        self.tab.append(["MUC", cliq, cpt])
+        
+    def multiplicationPotentiels(self, cliq1, parcliq2):
+        self.tab.append(["MUL", cliq1, parcliq2])
+        
+        
+    def marginalisation(self, cliq1, seloncliq2):
+        self.tab.append(["MAR", cliq1, seloncliq2])
+       
+    def normalisation(self, cliq):
+        self.tab.append(["NOR", cliq])
+        
+    def getTab(self):
+        return self.tab
+    
+    
