941e7f620e76cc8db6d54979209895eec5e58b5f
pierrestefani <poubelledepierre@gmail.com>
2015-05-30 17:03:04 +0200

    Fichier de test, Implémentation des tests à venir

--- testMeta.py
+++ testMeta.py
@@ -4,12 +4,29 @@ Created on Wed Apr 22 14:00:38 2015
 
 @author: Marvin
 """
+"""FICHIER TEST"""
+"""
+import Compilator as compilator
+from debugGenerator import debugGenerator
+import pyAgrum as gum
+import os
+os.chdir("C:/Users/Marvin/Desktop/Informatique/Projet PIMA/testMetaBaysGen/Recent2")
+
+bn = gum.loadBN("C:/Users/Marvin/Desktop/Informatique/Projet PIMA/testMetaBaysGen/BNs/Asia.bif")
+target = ["bronchitis?","visit_to_Asia?"]
+evs = {"smoking?":[1,0]}
+
+comp = compilator.compil(bn, target, evs)
+generator = debugGenerator()
+generator.genere(comp, "test.py", "getValue")
+"""
 
 
-from Compiler.Compiler import compil
+from Compiler import Compiler
 from Generator.pyAgrumGenerator import pyAgrumGenerator
 import pyAgrum as gum
-
+#import os
+#os.chdir("C:/Users/Marvin/Desktop/Informatique/Projet PIMA/testMetaBaysGen/Recent2")
 
 bn=gum.BayesNet()
 a,b,c,d,e=[bn.add(gum.LabelizedVariable(s,s,2)) for s in 'abcde']
@@ -23,7 +40,6 @@ bn.generateCPTs()
 targets = ["d","a"]
 evs = {"e":[1,0], "b":[0.25,0.75]}
 
-
 print("** Version aGrUM **")
 ie=gum.LazyPropagation(bn)
 ie.setEvidence(evs)
@@ -32,16 +48,13 @@ for t in targets:
     print(ie.posterior(bn.idFromName(t)))
 
 print("** Génération pyAgrum **")
-
-comp = compil(bn, targets[:], evs)
+comp = Compiler.compil(bn, targets[:], evs)
 generator = pyAgrumGenerator()
 generator.genere(bn, targets, evs, comp, "generated.py", "getValue")
 from generated import getValue
-getValue(bn,evs)
-
-for i in getValue(bn,evs):
-    print(i)
+print(getValue(evs))
+#for i in getValue(bn,evs):
+#    print(i)
 
 print("** Génération PHP **")
 print("to be done")
-
